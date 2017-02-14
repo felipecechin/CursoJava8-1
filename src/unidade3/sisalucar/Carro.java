3e829cbb1b906150984321eefa1b74c25fc0748b
Felipe Cechin Mello <ficechin@hotmail.com>
2017-02-13 22:31:03 -0200

    Pacote sisalucar atualizado e Conta.java adicionado.

--- src/unidade3/sisalucar/Carro.java
+++ src/unidade3/sisalucar/Carro.java
@@ -1,10 +1,33 @@
 package unidade3.sisalucar;
 
 public class Carro {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	long idCarro;
+	String placa;
+	String fabricante;
+	String modelo;
+	int ano;
+	String cor;
+	float valorDiaria;
+	
+	public Carro(long idCarro, String placa, String fabricante, String modelo, int ano, String cor, float valorDiaria) {
+		super(); //chama o construtor da superclasse
+		this.idCarro = idCarro;
+		this.placa = placa;
+		this.fabricante = fabricante;
+		this.modelo = modelo;
+		this.ano = ano;
+		this.cor = cor;
+		this.valorDiaria = valorDiaria;
 	}
-
+	
+	public Carro(){
+		
+	}
+	
+	public static void main(String[] args) {
+		Carro carro1 = new Carro(1,"ABC-2001","Volks","Gol",2013,"azul",99.90f);
+		Carro carro2 = new Carro();
+	}
+	
+	
 }
--- src/unidade3/sisalucar/SisalucarApp.java
+++ src/unidade3/sisalucar/SisalucarApp.java
@@ -1,14 +1,43 @@
 package unidade3.sisalucar;
 
+import java.time.LocalDate;
+
 public class SisalucarApp {
 
 	public static void main(String[] args) {
 		Carro carro1 = new Carro(); //objeto carro
+		carro1.idCarro = 1;
+		carro1.modelo = "gol";
+		carro1.placa = "jdk-0001";
+		carro1.cor = "azul";
+		carro1.ano = 2013;
+		carro1.valorDiaria = 99.90f;
+		carro1.fabricante = "volkswagen";
+		
 		Carro carro2 = new Carro();
 		
 		Cliente cliente1 = new Cliente();
+		cliente1.idCliente = 1;
+		cliente1.cpf = "12345678-90";
+		cliente1.nome = "João Pedro";
+		cliente1.cnh = "XYZ0001";
+		
 		Cliente cliente2 = new Cliente();
 		
+	}
+	
+	public void realizarLocacao(long idCarro, long idCliente) {
+		Locacao locacao = new Locacao();
+		locacao.idCarro = idCarro;
+		locacao.idCliente = idCliente;
+		locacao.idLocacao = 1;
+		locacao.datainicial = LocalDate.now();
+		locacao.datafinal = LocalDate.now().plusDays(2);
+		locacao.valorLocado = 2* 99.90f;
+	}
+	
+	public void gerarRelatorioLocacao(LocalDate ...datas) { //exemplo de varargs
+		float totalFaturado = 2 * 99.90f;
 	}
 
 }
